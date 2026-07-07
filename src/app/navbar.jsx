0b24a5ae10013ad4c419b733294dc3389f073554
Igor da Silva Dias <rogigor1712@gmail.com>
2026-07-06 21:15:17 -0300

    formatei

--- src/app/navbar.jsx
+++ src/app/navbar.jsx
@@ -1,17 +1,34 @@
-'use client'
+"use client";
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function Navbar() {
-    const pathname = usePathname()
+    const pathname = usePathname();
     return (
         <div>
             <nav>
-                <Link href={'/'} className={pathname === '/' ? "ativo" : ""}>Início</Link>
-                <Link href={'/equipe'} className={pathname === '/equipe' ? "ativo" : ""}>Equipe</Link>
-                <Link href={'/pesquisas'} className={pathname === '/pesquisas' ? "ativo" : ""}>Pesquisas</Link>
-                <Link href={'/graficos'} className={pathname === '/graficos' ? "ativo" : ""}>Gráficos</Link>
+                <Link href={"/"} className={pathname === "/" ? "ativo" : ""}>
+                    Início
+                </Link>
+                <Link
+                    href={"/equipe"}
+                    className={pathname === "/equipe" ? "ativo" : ""}
+                >
+                    Equipe
+                </Link>
+                <Link
+                    href={"/pesquisas"}
+                    className={pathname === "/pesquisas" ? "ativo" : ""}
+                >
+                    Pesquisas
+                </Link>
+                <Link
+                    href={"/graficos"}
+                    className={pathname === "/graficos" ? "ativo" : ""}
+                >
+                    Gráficos
+                </Link>
             </nav>
         </div>
     );
