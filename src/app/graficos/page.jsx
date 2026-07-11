ad9670d55ed83e74e308f14e8195eddce8338609
Igor da Silva Dias <rogigor1712@gmail.com>
2026-07-11 12:57:28 -0300

    feat: novas medições

--- src/app/graficos/page.jsx
+++ src/app/graficos/page.jsx
@@ -40,8 +40,20 @@ export default function Graficos() {
                             <td>72%</td>
                             <td>19.4°C</td>
                             <td>71.7%</td>
-                            <td></td>
-                            <td></td>
+                            <td>15°C</td>
+                            <td>74%</td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <td>Estacionamento</td>
+                            <td>10/07/2026</td>
+                            <td>20.1°C</td>
+                            <td>66%</td>
+                            <td>18.2°C</td>
+                            <td>85.9%</td>
+                            <td>15°C</td>
+                            <td>89%</td>
                         </tr>
                     </tbody>
                 </table>
@@ -49,6 +61,7 @@ export default function Graficos() {
                     <caption>Semana 2</caption>
                     <thead>
                         <tr>
+                            <th>LOCAL</th>
                             <th>DATA</th>
                             <th>TEMPERATURA (KIT)</th>
                             <th>UMIDADE (KIT)</th>
@@ -63,6 +76,7 @@ export default function Graficos() {
                     <caption>Semana 3</caption>
                     <thead>
                         <tr>
+                            <th>LOCAL</th>
                             <th>DATA</th>
                             <th>TEMPERATURA (KIT)</th>
                             <th>UMIDADE (KIT)</th>
