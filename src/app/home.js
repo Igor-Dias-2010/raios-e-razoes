a72c3efec04fa2159d6f0a9a5a2b82bd20513caa
Igor da Silva Dias <rogigor1712@gmail.com>
2026-07-22 13:52:22 -0300

    aplica mudançã de compressão de texto somente ao parágrafo da página início

--- src/app/home.js
+++ src/app/home.js
@@ -4,7 +4,7 @@ export default function Home() {
         <div>
             <Navbar />
             <h1>Raios e Razões</h1>
-            <p>
+            <p className="p-inicio">
                 Esse é um trabalho de matemática onde iremos analisar a
                 temperatura e umidade do estacionamento da escola SESI de
                 Florianópolis.
